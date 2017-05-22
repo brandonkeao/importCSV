8444eca3fbe0101898f64676e820bc6d1a5e3385
brandonkeao <brandon.keao@gmail.com>
2017-05-22 13:57:53 -0600

    Updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'results/index'
+  resources :results do
+    collection { post :import_csv }
+  end
+  
+root to: "users#index"
 
-  get 'results/accept_csv'
+  #get 'results/index'
+  #get 'results/accept_csv'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
